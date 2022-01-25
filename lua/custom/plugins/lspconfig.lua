6f44d269a84be7de3d593297ae8a708834264128
Huy Duong <huy.duong@employmenthero.com>
2022-01-25 09:57:13 +0700

    Remove diagnosticls

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,76 +3,17 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
-   lspconfig.solargraph.setup {
-      on_attach = attach,
-      capabilities = capabilities,
-      flags = {
-         debounce_text_changes = 150,
-      },
-   }
+   local servers = { "solargraph", "tsserver", "jdtls" }
 
-   lspconfig.tsserver.setup {
-      on_attach = attach,
-      capabilities = capabilities,
-      flags = {
-         debounce_text_changes = 150,
-      },
-      handlers = {
-         ["textDocument/publishDiagnostics"] = function() end,
-      },
-   }
-
-   lspconfig.diagnosticls.setup {
-      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "css" },
-      init_options = {
-         filetypes = {
-            javascript = "eslint",
-            typescript = "eslint",
-            javascriptreact = "eslint",
-            typescriptreact = "eslint",
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = on_attach,
+         capabilities = capabilities,
+         flags = {
+            debounce_text_changes = 150,
          },
-         linters = {
-            eslint = {
-               sourceName = "eslint",
-               command = "./node_modules/.bin/eslint",
-               rootPatterns = {
-                  ".eslitrc.js",
-                  "package.json",
-               },
-               debounce = 100,
-               args = {
-                  "--cache",
-                  "--stdin",
-                  "--stdin-filename",
-                  "%filepath",
-                  "--format",
-                  "json",
-               },
-               parseJson = {
-                  errorsRoot = "[0].messages",
-                  line = "line",
-                  column = "column",
-                  endLine = "endLine",
-                  endColumn = "endColumn",
-                  message = "${message} [${ruleId}]",
-                  security = "severity",
-               },
-               securities = {
-                  [2] = "error",
-                  [1] = "warning",
-               },
-            },
-         },
-      },
-   }
-
-   lspconfig.jdtls.setup {
-      on_attach = attach,
-      capabilities = capabilities,
-      flags = {
-         debounce_text_changes = 150,
-      },
-   }
+      }
+   end
 end
 
 return M
