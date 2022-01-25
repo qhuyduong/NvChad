8dad9a7324ddb8dfc67cd8776122e5a8aa1547cd
Huy Duong <huy.duong@employmenthero.com>
2022-01-25 10:28:43 +0700

    Update null-ls

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -2,16 +2,13 @@ local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
-   b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "javascript" } },
-   b.formatting.deno_fmt,
-
-   -- Lua
+   -- Formatting
    b.formatting.stylua,
-   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+   b.formatting.prettier,
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+   -- Diagnostics
+   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+   b.diagnostics.eslint,
 }
 
 local M = {}
@@ -23,7 +20,7 @@ M.setup = function()
       -- format on save
       on_attach = function(client)
          if client.resolved_capabilities.document_formatting then
-            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()"
          end
       end,
    }
